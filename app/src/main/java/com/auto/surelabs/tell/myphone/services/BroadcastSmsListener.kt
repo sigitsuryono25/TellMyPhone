4aee56de553d9f8d9d1b72b9a1447811758e949f
sigit <sigitharsy25@gmail.com>
2020-10-04 21:33:00 +0700

    fixing location update (remove location updates)

--- app/src/main/java/com/auto/surelabs/tell/myphone/services/BroadcastSmsListener.kt
+++ app/src/main/java/com/auto/surelabs/tell/myphone/services/BroadcastSmsListener.kt
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.telephony.SmsManager
 import android.telephony.SmsMessage
 import android.util.Log
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.auto.surelabs.tell.myphone.MainActivity
 import com.auto.surelabs.tell.myphone.R
@@ -18,7 +20,11 @@ import com.google.android.gms.location.*
 class BroadcastSmsListener : BroadcastReceiver() {
     private var context: Context? = null
     private var mediaPlayer: MediaPlayer? = null
+
     private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    private lateinit var mLocationCallback: LocationCallback
+    private var mIsReceivingUpdates = false
+    private lateinit var callback: UserLocationCallback
 
     override fun onReceive(context: Context?, intent: Intent?) {
         this.context = context
@@ -28,6 +34,7 @@ class BroadcastSmsListener : BroadcastReceiver() {
             Context.MODE_PRIVATE
         )
         val tujuan = prefs?.getString("no_tel", "")
+        callback = UserLocationCallback()
         if (intent?.action?.equals("android.provider.Telephony.SMS_RECEIVED") == true) {
             val bundler = intent.extras
             val msgs: Array<SmsMessage?>
@@ -51,10 +58,9 @@ class BroadcastSmsListener : BroadcastReceiver() {
         }
     }
 
-    private fun doAction(params: String?, msgFrom: String?) {
+    private fun doAction(params: String?, tujuan: String?) {
         if (params.equals("req ringing", true)) {
             val am = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-//                    am.ringerMode = AudioManager.RINGER_MODE_NORMAL
             am.setStreamVolume(AudioManager.STREAM_MUSIC, 100, 0)
             if (mediaPlayer != null) {
                 mediaPlayer?.release()
@@ -67,48 +73,77 @@ class BroadcastSmsListener : BroadcastReceiver() {
             Log.d("REQ", "RINGING")
 
         } else if (params.equals("req loc", true)) {
-            updateLocation(context, msgFrom)
+            getCurrentLocationUpdates(context, callback, tujuan)
         } else if (params.equals("req stop ringing", true)) {
             if (mediaPlayer != null) {
                 mediaPlayer?.stop()
                 mediaPlayer?.release()
             }
         } else if (params.equals("req stop loc", true)) {
-            updateLocation(context, msgFrom, true)
+            getCurrentLocationUpdates(context, callback, tujuan)
         }
     }
 
-    private fun updateLocation(context: Context?, msgFrom: String?, isCancel: Boolean = false) {
-        val loc = LocationRequest()
-        loc.apply {
+    private fun getCurrentLocationUpdates(
+        context: Context?,
+        callback: UserLocationCallback,
+        tujuan: String?
+    ) {
+        if (mIsReceivingUpdates) {
+            callback.onFailedRequest("Device is already receiving updates")
+            return
+        }
+
+        // Set up the LocationCallback for the request
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                callback.onLocationResult(locationResult?.lastLocation, tujuan)
+            }
+        }
+
+        // Start the request
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.apply {
             interval = 1000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
-        val permissionCheckedInputStream = ContextCompat.checkSelfPermission(
+        val checkPermission = ContextCompat.checkSelfPermission(
             context,
             android.Manifest.permission.ACCESS_FINE_LOCATION
         )
-        if (permissionCheckedInputStream == PackageManager.PERMISSION_GRANTED) {
+        if (checkPermission == PackageManager.PERMISSION_GRANTED) {
             mFusedLocationProviderClient?.requestLocationUpdates(
-                loc,
-                MyLocationCallback(msgFrom),
+                mLocationRequest,
+                mLocationCallback,
                 null
             )
         }
-        mFusedLocationProviderClient?.removeLocationUpdates(MyLocationCallback(msgFrom))
+        // Update the request state flag
+        mIsReceivingUpdates = true
     }
 
-}
+    private fun stopLocationUpdates() {
+        mFusedLocationProviderClient?.removeLocationUpdates(mLocationCallback)
+        mIsReceivingUpdates = false
+        Log.i("Remove Location", "Location updates removed")
+    }
 
-class MyLocationCallback(private val msgFrom: String?) : LocationCallback() {
-    override fun onLocationResult(p0: LocationResult?) {
-        super.onLocationResult(p0)
-        val lat = p0?.lastLocation?.latitude
-        val lon = p0?.lastLocation?.longitude
-        val url = "http://www.google.com/maps/place/$lat,$lon"
-        val mSmsManager = SmsManager.getDefault()
-        mSmsManager.sendTextMessage(msgFrom, null, "Here's the phone location $url", null, null)
-        Log.d("REQ", "$msgFrom")
+    inner class UserLocationCallback {
+        fun onLocationResult(location: Location?, tujuan: String?) {
+            val url =
+                "http://www.google.com/maps/place/${location?.latitude},${location?.longitude}"
+            val mSmsManager = SmsManager.getDefault()
+            mSmsManager.sendTextMessage(tujuan, null, "Here's the phone location $url", null, null)
+            Log.d("REQ", "$tujuan")
+
+            stopLocationUpdates()
+        }
+
+        fun onFailedRequest(message: String?) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
